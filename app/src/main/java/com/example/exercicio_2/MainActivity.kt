7e6a1d968f6ac34299b593340470af022bb33916
Gabriel Poloni <gpparaestudos@gmail.com>
2025-08-28 14:39:00 -0300

    Alteração da caixa de texto.

--- app/src/main/java/com/example/exercicio_2/MainActivity.kt
+++ app/src/main/java/com/example/exercicio_2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.exercicio_2
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,24 +10,21 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.exercicio_2.databinding.ActivityMainBinding
 
 
-
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "INFOTESTE"
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val nome = "Nome do Curso"
         binding.txtnome.text= nome
 
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.i(TAG, "Oncreate")
     }
 }
